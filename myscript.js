62131e4045ec3dfa958d6313d3a0d0927d5bb96b
Regina Jackson <rjackson156@gmail.com>
2022-03-28 19:36:07 -0400

    add eventListener for mouseover to the squares

--- myscript.js
+++ myscript.js
@@ -2,8 +2,13 @@ document.body.onload = addElement
 
 function addElement() {
     for (let i = 0; i < 256; i++){
-        const grid = document.querySelector(".gridContainer")
-        const div = document.createElement("div")
+        var grid = document.querySelector(".gridContainer")
+        var div = document.createElement("div")
         div.classList.add("square")
         grid.appendChild(div)
 }}
+
+const square = document.querySelector("div")
+square.addEventListener('mouseover', function(event) {
+    event.target.classList.replace("square", "change")
+})
