c46282686f809ef29cba9bae836c832eec419399
Regina Jackson <rjackson156@gmail.com>
2022-03-29 20:43:04 -0400

    add functionality to the reset button

--- myscript.js
+++ myscript.js
@@ -1,9 +1,12 @@
 document.body.onload = addElement
 
+const button = document.querySelector("button")
+const grid = document.querySelector(".gridContainer")
+
+
 function addElement() {
-    for (let i = 0; i < 100; i++){
-        var grid = document.querySelector(".gridContainer")
-        var div = document.createElement("div")
+    for (let i = 0; i < 256; i++){
+        const div = document.createElement("div")
         div.classList.add("square")
         grid.appendChild(div)
 }}
@@ -12,3 +15,12 @@ const square = document.querySelector("div")
 square.addEventListener('mouseover', function(event) {
     event.target.classList.replace("square", "change")
 })
+
+
+button.addEventListener('click', reset)
+function reset(){
+    grid.innerHTML = "";
+    grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+    addElement()
+}
